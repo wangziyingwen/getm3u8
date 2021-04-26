2a25614006676e79c9d3424464d23025aa854417
wangziyingwen <wz.lxh@outlook.com>
2021-04-27 00:09:10 +0800

    1

--- Spider/main.py
+++ Spider/main.py
@@ -1,15 +1,42 @@
+from browsermobproxy import Server
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import os
+import os,time,sys
+
+path=sys.path[0]+r'/chrome/browsermob-proxy'
+server = Server(path)
+server.start()
+proxy = server.create_proxy()
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--proxy-server={0}'.format(proxy.proxy))
 chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
+
 driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
-driver.get("https://www.baidu.com")
+#要访问的地址
+base_url = "https://zh.cam4.com/asiart"
+proxy.new_har("cam4", options={ 'captureContent': True})
+
+driver.get(base_url)
+
+#此处最好暂停几秒等待页面加载完成，不然会拿不到结果
+time.sleep(5)
+result = proxy.har
 print(driver.title)
+for entry in result['log']['entries']: 
+    _url = entry['request']['url']
+    # 根据URL找到数据接口,这里要找的是 http://git.liuyanlin.cn/get_ht_list 这个接口 
+    if "m3u8" in _url: 
+	    print(_url) 
+        a_response = entry['response'] 
+        b_content = _response['content'] 
+    # 获取接口返回内容 
+        print(a_response)
+
+server.stop()
 driver.quit()
